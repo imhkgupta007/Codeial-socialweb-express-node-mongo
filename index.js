9ec0b2921731419ee495d1eec88b5302722e0dd9
Harshit <gupta.harshhhhhit@gmail.com>
2020-07-26 00:47:01 +0530

    View Set up

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // Use express router
 app.use('/', require('./routes'));
 
+// Set up view Engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running ${err}`);
